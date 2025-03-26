25b3889842771f5e178b5ce28b92509216d3b9dd
Ash0raf007 <Ashrafkadry07@gmail.com>
2025-03-26 22:40:17 +0200

    first commit

---
+++ constants/images.ts
@@ -0,0 +1,10 @@
+// import all images here and export them as an object to be used in the app
+import bg from "@/assets/images/bg.png";
+import highlight from "@/assets/images/highlight.png";
+import rankingGradient from "@/assets/images/rankingGradient.png";
+
+export const images = {
+  bg,
+  highlight,
+  rankingGradient,
+};
